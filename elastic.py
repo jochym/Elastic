6646cfffc3e9feadaa0e425d5d3ef0ae99ba67de
Paweł T. Jochym <Pawel.Jochym@ifj.edu.pl>
2011-07-23 02:24:56 +0200

    Better (standard) copyright notices.

--- elastic.py
+++ elastic.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python
 
 #
-# Copyright 1998-2011 by Paweł T. Jochym <pawel.jochym@ifj.edu.pl>
+#    Copyright 1998-2011 by Paweł T. Jochym <pawel.jochym@ifj.edu.pl>
 #
-# This code is licensed under Gnu Public License (GPL) v3 or later.
-#
+#    This file is part of Elastic.
+
+#    Elastic is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    Elastic is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Elastic.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import re
 import sys
--- pvasp.py
+++ pvasp.py
@@ -3,8 +3,20 @@
 #
 # Copyright 2011 by Paweł T. Jochym <pawel.jochym@ifj.edu.pl>
 #
-# This code is licensed under Gnu Public License (GPL) v3 or later.
-#
+#    This file is part of Elastic.
+
+#    Elastic is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    Elastic is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Elastic.  If not, see <http://www.gnu.org/licenses/>.
 
 #
 # Parallel vasp calculator using mulitprocessing.
