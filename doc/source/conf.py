736b83f78f0316584344bb4cf4457a0389187fd0
Paweł T. Jochym <Pawel.Jochym@ifj.edu.pl>
2011-08-13 18:36:07 +0200

    Switch theme to haiku. Latex page to a4.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'haiku'
 
 jsmath_path = 'jsmath/easy/load.js'
 
@@ -172,7 +172,7 @@ htmlhelp_basename = 'Elasticdoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 # The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
+latex_paper_size = 'a4'
 
 # The font size ('10pt', '11pt' or '12pt').
 #latex_font_size = '10pt'
