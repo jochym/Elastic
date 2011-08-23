b339cd3a6da912e6a6a8884408c3171ff19641ff
Paweł T. Jochym <Pawel.Jochym@ifj.edu.pl>
2011-08-23 14:48:59 +0200

    Switch to sphinx template.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -92,7 +92,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'haiku'
+html_theme = 'sphinxdoc'
 
 jsmath_path = 'jsmath/easy/load.js'
 
