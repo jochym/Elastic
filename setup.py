aae60bc47e73d12e7402cfb913176c59e42daa70
Paweł T. Jochym <pawel.jochym@ifj.edu.pl>
2014-01-10 13:11:35 -0600

    PyPi version bump to 4.0.2.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 
 setup(
     name='elastic',
-    version='4.0.1',
+    version='4.0.2',
     packages=['elastic',],
     license='GPLv3',
     description = 'Extension for ASE to calculate elastic constants',
     author = 'Paweł T. Jochym',
     author_email = 'Pawel.Jochym@ifj.edu.pl',
     url = 'https://github.com/jochym/Elastic',   # use the URL to the github repo
-    download_url = 'https://github.com/jochym/Elastic/tarball/0.3.2', # I'll explain this in a second
+    download_url = 'https://github.com/jochym/Elastic/tarball/4.0.2', # I'll explain this in a second
     keywords = ['science', 'physics', 'ase', 'elastic constants', 'crystals'], # arbitrary keywords
     classifiers = [],
 )
