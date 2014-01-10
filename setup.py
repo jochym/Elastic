77fa7d90794fbfbb9a2fb05f48242c7f1214f8bd
Paweł T. Jochym <pawel.jochym@ifj.edu.pl>
2014-01-10 13:00:06 -0600

    Setup of the 4.0.1 version. PyPi config. Moving files around. Readme.

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='qeutil',
+    version='0.1.8',
+    packages=['qeutil',],
+    license='GPLv3',
+    description = 'A set of utilities for Quantum Espresso',
+    author = 'Paweł T. Jochym',
+    author_email = 'Pawel.Jochym@ifj.edu.pl',
+    url = 'https://github.com/jochym/qe-util',   # use the URL to the github repo
+    download_url = 'https://github.com/jochym/qe-util/tarball/0.1.4', # I'll explain this in a second
+    keywords = ['science', 'physics', 'quantum-espresso'], # arbitrary keywords
+    classifiers = [],
+)
