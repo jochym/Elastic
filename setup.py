980bba3cd95899d21dcbf7732322fadc91931fc1
Paweł T. Jochym <pawel.jochym@ifj.edu.pl>
2014-05-21 23:43:23 +0200

    Clean-up of setup for proper conda building.

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ from distutils.core import setup
 
 setup(
     name='elastic',
-    version='4.0.2',
-    packages=['elastic',],
+    version='4.0.5',
+    packages=['elastic'],
     license='GPLv3',
     description = 'Extension for ASE to calculate elastic constants',
     author = 'Paweł T. Jochym',
     author_email = 'Pawel.Jochym@ifj.edu.pl',
-    url = 'https://github.com/jochym/Elastic',   # use the URL to the github repo
-    download_url = 'https://github.com/jochym/Elastic/tarball/4.0.2', # I'll explain this in a second
-    keywords = ['science', 'physics', 'ase', 'elastic constants', 'crystals'], # arbitrary keywords
+    url = 'https://github.com/jochym/Elastic',
+    download_url = 'https://github.com/jochym/Elastic/tarball/4.0.2', 
+    keywords = ['science', 'physics', 'ase', 'elastic constants', 'crystals'],
+    requires = ['pyspglib','numpy','scipy','ase'],
+    provides = ['elastic'],
+    platforms = ['all'],
     classifiers = [],
 )
