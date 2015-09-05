57928093e95864d5bed1d6d0c286bb6ab0c04774
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2015-09-06 00:04:46 +0200

    Try to move to dynamic version numbers from scm.

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from distutils.core import setup
 
 setup(
     name='elastic',
-    version='4.1.0_dev',
     packages=['elastic'],
     license='GPLv3',
     description = 'Extension for ASE to calculate elastic constants',
     author = 'Paweł T. Jochym',
     author_email = 'Pawel.Jochym@ifj.edu.pl',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     url = 'https://github.com/jochym/Elastic',
     keywords = ['science', 'physics', 'ase', 'elastic constants', 'crystals'],
     requires = ['pyspglib','numpy','scipy','ase'],
