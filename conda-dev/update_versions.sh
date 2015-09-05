63f6b208ef681d49ada19a218a3a4cff02633dc9
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2015-09-06 01:23:51 +0200

    Move to setuptools. Put current version in docs.

--- conda-dev/update_versions.sh
+++ conda-dev/update_versions.sh
@@ -2,7 +2,8 @@
 
 VER=`(cd .. ; python -c "from setuptools_scm import get_version ; print(get_version())")`
 
-echo $VER
+echo "Building version: $VER"
 
-sed "s/version: XXXX/version: $VER/g" <meta.yaml.tmpl >meta.yaml
+sed "s/version: XXXX/version: \"$VER\"/g" <meta.yaml.tmpl >meta.yaml
 
+sed "s/version = XXXX/version = \"$VER\"/g" <../doc/source/conf.py.tmpl  >../doc/source/conf.py
