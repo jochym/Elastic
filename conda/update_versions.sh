7fd12e20f706dc5411cd182a1df8d8fd2dada82e
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2015-09-06 04:24:07 +0200

    Add version tracking file to trigger CI builds.

--- conda/update_versions.sh
+++ conda/update_versions.sh
@@ -7,3 +7,6 @@ echo "Building version: $VER"
 sed "s/version: XXXX/version: \"$VER\"/g" <meta.yaml.tmpl >meta.yaml
 
 sed "s/version = XXXX/version = \"$VER\"/g" <../docs/source/conf.py.tmpl  >../docs/source/conf.py
+
+echo $VER >VERSION
+
