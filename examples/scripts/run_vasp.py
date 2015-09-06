d3d5455f68c5699e7a9cede4ec585bac753c6b90
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2015-09-06 12:29:11 +0000

    Add example site scripts.

---
+++ examples/scripts/run_vasp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import os
+exitcode = os.system('run-on-tsi ~/bin/run-vasp5-lock >/dev/null')
+#exitcode = os.system('run-on-tsi /opt/bin/run-vasp5 >/dev/null')
+
+#exitcode = os.system('~/bin/run-vasp5-lock')
