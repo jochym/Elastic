a5d522a892b7d1b4c463ad28147303c82e7b6445
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2015-12-18 14:53:50 +0100

    Add pypi target(?).

--- conda/build.sh
+++ conda/build.sh
@@ -3,6 +3,7 @@
 git describe --tags --dirty | sed -e 's/-/_/g' -e 's/^v//g' > __conda_version__.txt
 
 $PYTHON setup.py install
+$PYTHON setup.py sdist
 
 # Add more build steps here, if they are necessary.
 
