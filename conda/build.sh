40e7b686b04ac693ae3ac68f8eef3671ef95da73
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2016-01-11 09:34:05 +0100

    Switch to release+patchlevel versioning scheme.

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git describe --tags --dirty | sed -e  's/-/+/' -e 's/-/./' -e 's/^v//'> __conda_version__.txt
+git describe --tags --dirty | sed -e 's/-\(.*\)-g.*/+\1/' -e 's/^v//g' > __conda_version__.txt
 
 $PYTHON setup.py install
 #$PYTHON setup.py sdist
