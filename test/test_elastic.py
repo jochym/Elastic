958eba6e15c6cd7142a815e8841ada24a40179a6
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2016-03-07 21:49:17 +0100

    Test clean up.

--- test/test_elastic.py
+++ test/test_elastic.py
@@ -11,13 +11,9 @@ class TestElastic(unittest.TestCase):
     
     def test_elastic_import(self):
 	    import elastic
-	    print('Imported:',elastic.__package__)
 
     def test_parcalc_import(self):
 	    import parcalc
-	    print('Imported:',parcalc.__package__)
 
 if __name__ == '__main__':
-#    suite = unittest.TestLoader().loadTestsFromTestCase(TestElastic)
-#    unittest.TextTestRunner(verbosity=2).run(suite)
      unittest.main()
