d8e7215ead37dae59a2a22fe2bee8ec4285e2c79
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2016-03-07 20:56:01 +0100

    Add basic test case for travis.

---
+++ test/test_elastic.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestElastic(unittest.TestCase):
+
+    def setUp(self):
+	pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_basic_import(self):
+	import elastic
+	import parcalc
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestElastic)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    # unittest.main()
