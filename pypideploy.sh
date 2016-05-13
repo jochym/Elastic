801206073f99fb5d431f99a1f35167e0bd7ae226
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2016-05-13 04:48:27 +0200

    Try script deploynment

---
+++ pypideploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+srv=$1
+shift
+
+cat  >~/.pypirc <<-EOF 
+
+    [distutils]
+    index-servers =
+        pypi
+        pypitest
+
+    [pypi]
+    repository = https://pypi.python.org/pypi
+    username:$USER
+    password:$PASSWORD
+
+    [pypitest]
+    repository = https://testpypi.python.org/pypi
+    username:$USER
+    password:$TESTPASSWORD
+
+EOF
+
+
+python setup.py register -r $srv
+python setup.py $* upload -r $srv
