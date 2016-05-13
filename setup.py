ae3087d8183a19910246ad7a138744cca658e542
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2016-05-13 18:39:56 +0200

    Block upload_docs in setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from sys import argv
 
-with open("__version__.txt") as w:
-    for line in w:
-        version = line.strip()
-        break
+if 'upload_docs' in argv :
+    print('upload_docs is not supported by this package.')
+else :
+    from setuptools import setup, find_packages
 
-setup(
-    name='elastic',
-    version=version, 
-    packages=find_packages(exclude=['docs']),
-    license='GPLv3',
-    description = 'Extension for ASE to calculate elastic constants',
-    author = 'Paweł T. Jochym',
-    author_email = 'Pawel.Jochym@ifj.edu.pl',
-    url = 'https://github.com/jochym/Elastic',
-    keywords = ['science', 'physics', 'ase', 'elastic constants', 'crystals'],
-    requires = ['spglib','numpy','scipy','ase','docutils','sphinx'],
-    setup_requires = ['docutils','sphinx','setuptools_scm'],
-    provides = ['elastic','parcalc'],
-    platforms = ['all'],
-    classifiers = [],
-)
+    with open("__version__.txt") as w:
+        for line in w:
+            version = line.strip()
+            break
+
+    setup(
+        name='elastic',
+        version=version, 
+        packages=find_packages(exclude=['docs']),
+        license='GPLv3',
+        description = 'Extension for ASE to calculate elastic constants',
+        author = 'Paweł T. Jochym',
+        author_email = 'Pawel.Jochym@ifj.edu.pl',
+        url = 'https://github.com/jochym/Elastic',
+        keywords = ['science', 'physics', 'ase', 'elastic constants', 'crystals'],
+        requires = ['spglib','numpy','scipy','ase','docutils','sphinx'],
+        setup_requires = ['docutils','sphinx','setuptools_scm'],
+        provides = ['elastic','parcalc'],
+        platforms = ['all'],
+        classifiers = [],
+    )
