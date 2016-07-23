1e52e2516077653826e4d5ac006aabee0a4fdf94
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2016-07-23 13:17:05 +0200

    Switch to new style git vars for version in conda.

--- conda/build.sh
+++ conda/build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-#git describe --tags --dirty | sed -e 's/-\(.*\)-g.*/+\1/' -e 's/^v//g' > __conda_version__.txt
-git describe --tags --dirty | sed -e 's/-/+/' -e 's/-/./' -e 's/^[vr]//' > __conda_version__.txt
-
 $PYTHON setup.py install
 #$PYTHON setup.py sdist
 
-# Add more build steps here, if they are necessary.
 
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
