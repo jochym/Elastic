9b5c5d8768d682b24944abdb52fd5d00f6869c03
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2017-09-14 00:03:56 +0200

    Display version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -101,7 +101,9 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'display_version': True,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
