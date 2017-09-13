b8c1e119954fcd470db0f586b9f62563a5ea473d
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2017-09-13 16:52:06 +0200

    Simplify versioning scheme

--- setup.py
+++ setup.py
@@ -3,21 +3,11 @@
 from setuptools import setup, find_packages
 from setuptools_scm import get_version
 
-ver = get_version().split('.')
-if len(ver) > 3:
-    # Post release version:
-    #  - add patchlevel at the end
-    #  - decrease back last number
-    print('Version from SCM:', ver)
-    try:
-        pl = int(ver[3].split('+')[0][3:])
-        ver[2] = str(int(ver[2])-1)
-        ver = ver[:3]
-        ver.append(str(pl))
-    except ValueError:
-        # This is probably build from pypi source
-        # Just keep the version from setuptools_scm and hope for the best
-        pass
+ver = get_version().split('+')[0].split('.')
+
+if ver[-1].startswith('dev'):
+    ver[-1] = ver[-1][3:]
+    ver[-2] = str(int(ver[-2])-1)
 
 ver = '.'.join(ver)
 
