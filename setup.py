c52129fc21c9ee13f6d859a959fa08e65ffba9a5
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2018-04-16 12:13:20 +0200

    Add long desc content type

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     license='GPLv3',
     description='Extension for ASE to calculate elastic constants',
     long_description=open('README.rst', 'rb').read().decode('utf-8'),
+    long_description_content_type='text/x-rst',
     author='Paweł T. Jochym',
     author_email='Pawel.Jochym@ifj.edu.pl',
     url='https://github.com/jochym/Elastic',
