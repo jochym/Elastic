fcfbb14b5bfdb3bb6a9c3dae6e1ab600cc8eb13f
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2024-04-15 19:19:15 +0200

    Fix scm ver imports

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from __future__ import annotations
 from setuptools import setup
 from setuptools_scm import ScmVersion
+from setuptools_scm.version import no_local_version
 
 
 def clean_version(version: ScmVersion) -> str:
     from setuptools_scm.version import guess_next_version
+    from setuptools_scm.version import only_version
     
     return version.format_next_version(#guess_next_version,
                                         only_version, 
