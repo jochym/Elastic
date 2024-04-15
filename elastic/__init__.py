5c4e2495da969e7ae5cf1285a847b92e017ad6b4
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2024-04-15 19:06:54 +0200

    Use generated _version file

--- elastic/__init__.py
+++ elastic/__init__.py
@@ -68,13 +68,4 @@ from .elastic import get_elementary_deformations, scan_volumes
 from .elastic import get_pressure, get_strain, BMEOS
 
 # To reach "elastic.__version__" attribute in other programs
-from setuptools_scm import get_version
-ver = get_version().split('+')[0].split('.')
-
-if ver[-1].startswith('dev'):
-    ver[-1] = ver[-1][3:]
-    ver[-2] = str(int(ver[-2])-1)
-
-ver = '.'.join(ver)
-
-__version__ = ver
+from ._version import __version__
