4adbc83257e2983a26b85ed78936159845396808
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2024-12-07 18:03:42 +0100

    Remove dev from distance

--- setup.py
+++ setup.py
@@ -17,15 +17,9 @@ def current_version(version: ScmVersion) -> str:
         delim = '-post'
 
     return version.format_next_version(guess_next_version,
+                                        "{tag}.{distance}")
+    return version.format_next_version(guess_next_version,
                                         "{tag}"+ delim +"{distance}")
 
-def myversion_func(version: ScmVersion) -> str:
-    from setuptools_scm.version import guess_next_version
-
-    return version.format_next_version(guess_next_version, "{tag}.{distance}")
-
-
-# setup(use_scm_version={"version_scheme": current_version, 
-#                         "local_scheme": get_no_local_node})
-
-setup(use_scm_version={"version_scheme": myversion_func})
+setup(use_scm_version={"version_scheme": current_version, 
+                        "local_scheme": get_no_local_node})
